96783a25104962a15c62ea7ba65f34648c0d37b5
Robbie Muchiri <developedbyrobbie@gmail.com>
2024-02-06 13:35:20 +0300

    aded vercel, code refactor

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,7 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const serverUrl = 'http://localhost:3000';
+export const serverUrl = 'https://the-ladder-up.onrender.com';
+// export const serverUrl = 'http://localhost:3000';
 
 export const categories = ['Music', 'Fashion', 'Films', 'Sports', 'Decor', 'Culinary'];
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const dotenv = require('dotenv').config();
-const { Sequelize } = require('sequelize');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const { sequelize } = require('./models');
@@ -14,6 +12,12 @@ app.use(express.json());
 app.use(cors());
 
 
+
+/* ROUTES */
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+
+
 /* SEQUELIZE SETUP */
 // sequelize.sync({ force: true })
 sequelize.sync()
@@ -23,12 +27,6 @@ sequelize.sync()
     .catch((err) => {
         console.error(err)
     })
-
-
-/* ROUTES */
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
